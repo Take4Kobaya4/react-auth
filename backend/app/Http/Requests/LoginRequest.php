02b6e9065feca09cc6671c5a94d48a0ac7c2491f
Takeshi Kobayashi <t.kobayashi3927@gmail.com>
2025-05-29 16:44:46 +0900

    バックエンドの認証機能作成

---
+++ backend/app/Http/Requests/LoginRequest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email',
+            'password' => 'required|string|min:8',
+        ];
+    }
+
+    public function messages() :array
+    {
+        return [
+            'email.required' => 'メールアドレスは必須です',
+            'email.email' => '有効なメールアドレスを入力してください',
+            'password.required' => 'パスワードは必須です',
+            'password.min' => 'パスワードは少なくとも8文字以上である必要があります',
+        ];
+    }
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param  \Illuminate\Contracts\Validation\Validator  $validator
+     * @return void
+     *
+     * @throws \Illuminate\Http\Exceptions\HttpResponseException
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'status' => 'error',
+            'message' => 'バリデーションエラー',
+            'errors' => $validator->errors(),
+        ], 422));
+    }
+}
